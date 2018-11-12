0d0d6e4eabbd0ab1e6b9520586715a3c587787a5
OSanchez98 <elilosanz@gmail.com>
2018-11-12 10:50:21 -0500

    Administradores controladores, modelo y rutas

--- src/Controllers/AdministradorCotroller.js
+++ src/Controllers/AdministradorCotroller.js
@@ -1,15 +1,43 @@
 const mongoose = require('mongoose');
-const Administrador = require('../Models/Administrador');
+const administrador = require('../Models/Administrador');
 
-exports.admin_lista = (req,res)=>{
-    Administrador.find({},(err,response)=>{
+exports.adminlista = (req,res)=>{
+    administrador.find({},(err,response)=>{
         if (err) return res.send( JSON.stringify({ Code: 500, Message: `Error Admin list: ${err}` }) )
         return res.send( JSON.stringify({
         Code: 200,
         Message: ` Administradores success`,
         Data: {
-            Administrador: response
+            Admin: response
           }
         }))
     })
+},
+
+exports.adminCreate=(req,res,next)=>{
+    console.log('consulting contact create ... ')
+
+    let Nombre = req.body.Nombre
+    let Apellidos = req.body.Apellidos
+    let Correo = req.body.Correo
+    let Pass = req.body.Pass
+
+    let AdminInstance = new administrador({
+        Nombre,
+        Apellidos,
+        Correo,
+        Pass
+    })
+
+    AdminInstance.save((err,response)=>{
+        if (err) return res.send( JSON.stringify({ Code: 500, Message: `Not Created Error: ${err}` }))
+        return res.send( JSON.stringify({
+            Code: 200,
+            Message: `New User and Contact CREATED!! - ID ${response._id}`,
+            Data: {
+              Id_Inserted: response._id,
+            }
+          }))
+    })
+
 }
--- src/Routes/AdministradorRoutes.js
+++ src/Routes/AdministradorRoutes.js
@@ -2,5 +2,6 @@ const AdministradorController = require('../Controllers/AdministradorCotroller')
 
 module.exports = function(app)
 {
-    app.post('/api/administrador/lista',AdministradorController.admin_lista);
+    app.post('/api/administrador/lista',AdministradorController.adminlista);
+    app.post('/api/administrador/create',AdministradorController.adminCreate);
 }
