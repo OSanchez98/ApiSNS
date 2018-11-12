81ba2bf87dbcc77e1247dfac9bf784f95fa91084
OSanchez98 <elilosanz@gmail.com>
2018-11-12 10:50:55 -0500

    Categorias modelo,controlador y rutas

--- src/Controllers/CategoriaController.js
+++ src/Controllers/CategoriaController.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const Categoria = require('../Models/Categorias');
+const Categoria = require('../models/Categorias');
 
-exports.categorias_lista = (req,res)=>{
+exports.categoriasLista = (req,res)=>{
     Categoria.find({},(err,response)=>{
-        console.log("repso:",response)
         if (err) return res.send( JSON.stringify({ Code: 500, Message: `Error Categorias list: ${err}` }) )
         return res.send( JSON.stringify({
         Code: 200,
@@ -13,4 +12,27 @@ exports.categorias_lista = (req,res)=>{
           }
         }))
     })
+}
+exports.categoriasCreate =(req,res,next)=>{
+    console.log('consulting Categoria create ... ')
+
+    let Code = req.body.Code
+    let Descripcion = req.body.Descripcion
+
+    let CategoriaInstance = new Categoria({
+        Code,
+        Descripcion
+    })
+
+    CategoriaInstance.save((err,response)=>{
+        if (err) return res.send( JSON.stringify({ Code: 500, Message: `Not Created Error: ${err}` }))
+        return res.send( JSON.stringify({
+            Code: 200,
+            Message: `New Categoria CREATED!! - ID ${response._id}`,
+            Data: {
+              Id_Inserted: response._id,
+            }
+          }))
+    })
+
 }
--- src/Routes/CategoriaRoutes.js
+++ src/Routes/CategoriaRoutes.js
@@ -2,5 +2,6 @@ const CategoriasController = require('../Controllers/CategoriaController');
 
 module.exports = function(app)
 {
-    app.post('/api/Categorias/lista',CategoriasController.categorias_lista);
+    app.post('/api/categorias/lista',CategoriasController.categoriasLista);
+    app.post('/api/categorias/create',CategoriasController.categoriasCreate);
 }
