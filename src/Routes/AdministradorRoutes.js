884757761137449d4bbac88845246e801f5d93fd
OSanchez98 <elilosanz@gmail.com>
2018-11-16 09:43:01 -0500

    administradores update listo

--- src/Routes/AdministradorRoutes.js
+++ src/Routes/AdministradorRoutes.js
@@ -4,4 +4,7 @@ module.exports = function(app)
 {
     app.post('/api/administrador/lista',AdministradorController.adminlista);
     app.post('/api/administrador/create',AdministradorController.adminCreate);
+    app.post('/api/administradores/update/:id',AdministradorController.adminUpdate);
+    app.post('/api/administradores/delete/:id',AdministradorController.adminDelete);
+    
 }
