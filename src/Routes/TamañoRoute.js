be82394d05fe13cba329d260b696557802fd2184
OSanchez98 <elilosanz@gmail.com>
2018-11-16 10:32:48 -0500

    Tamaño update y delete listo

--- src/Routes/TamañoRoute.js
+++ src/Routes/TamañoRoute.js
@@ -4,4 +4,6 @@ module.exports = function(app)
 {
     app.post('/api/tam/lista',TamañoController.tamañoLista);
     app.post('/api/tam/create',TamañoController.tamañoCreate);
+    app.post('/api/tam/update/:id',TamañoController.tamañoUpdate);
+    app.post('/api/tam/delete/:id',TamañoController.tamañoDelete);
 }
